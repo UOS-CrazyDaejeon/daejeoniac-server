b26044fb552d7432ff851724dc5d83466a8e10b5
ktollonr <agetor3210@naver.com>
2026-07-09 17:12:24 +0900

    initial spring boot project

---
+++ src/main/java/com/daejeongwang/uoscrazydaejeon/UosCrazyDaejeonApiApplication.java
@@ -0,0 +1,13 @@
+package com.daejeongwang.uoscrazydaejeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UosCrazyDaejeonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UosCrazyDaejeonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daejeongwang/uoscrazydaejeon/UosCrazyDaejeonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daejeongwang.uoscrazydaejeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UosCrazyDaejeonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
